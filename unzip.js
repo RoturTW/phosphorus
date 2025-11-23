8c70adc1dfaf46a2a6c7c50c1d0609547f2f1b15
Flufi-boi <floofster@flufi.gay>
2025-11-23 21:10:15 +0000

    yum

---
+++ unzip.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+const zip = JSON.parse(fs.readFileSync("zip.json", "utf-8"));
+
+function unzipFile(f, p) {
+    if (!fs.existsSync(p))
+        fs.mkdirSync(p);
+    
+    if (f[0] == ".folder") {
+        fs.mkdirSync(path.join(p, f[1]));
+        for (let i = 0; i < f[2].length; i++) {
+            unzipFile(f[2][i], path.join(p, f[1]));
+        }
+    } else {
+        fs.writeFileSync(path.join(p,`${f[1]}${f[0]}`), f[2]);
+    }
+}
+
+fs.rmSync("./phosphorus", { recursive: true });
+
+unzipFile(zip, "./");
